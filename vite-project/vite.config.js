a7a56d308f913f7d574df9a00bf56918b3fd2c01
GAQDT <guoaoqing666@gmail.com>
2025-08-29 20:00:22 +0800

    init docs

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../docs',   // 构建输出到仓库的 docs 文件夹
+    emptyOutDir: true    // 每次构建时清空 docs
+  }
 })
